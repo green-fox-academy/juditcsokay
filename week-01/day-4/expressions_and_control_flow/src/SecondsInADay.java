660dffd2c3bf0c20103a85f9eeb1a8f5799116a7
juditcsokay <juditcsokay@gmail.com>
2018-05-10 16:32:46 +0200

    SecondsInADay

--- week-01/day-4/expressions_and_control_flow/src/SecondsInADay.java
+++ week-01/day-4/expressions_and_control_flow/src/SecondsInADay.java
@@ -4,6 +4,13 @@ public class SecondsInADay {
         int currentMinutes = 34;
         int currentSeconds = 42;
 
+        int secondsPerDay = (24*60)*60;
+        System.out.println(secondsPerDay);
+
+        int passedseconds = (((14*60)+34)*60)+42;
+        int remainingSeconds = secondsPerDay - passedseconds;
+
+        System.out.println("The remaining seconds from the day: " + remainingSeconds);
         // Write a program that prints the remaining seconds (as an integer) from a
         // day if the current time is represented by the variables
     }
