dd13545fe77a8f5a65fa6b9ea98c02f56cb20c80
juditcsokay <juditcsokay@gmail.com>
2018-05-10 13:49:51 +0200

    CodingHours

---
+++ week-01/day-4/expressions_and_control_flow/src/HelloOthers.java
@@ -0,0 +1,13 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+        System.out.println("Hello, Evo!");
+        System.out.println("Hello, Gábor!");
+        System.out.println("Hello, Mirkó!");
+    }
+}
---
+++ week-01/day-4/expressions_and_control_flow/src/TwoNumbers.java
@@ -0,0 +1,33 @@
+public class TwoNumbers {
+    public static void main(String[] args) {
+        // Create a program that prints a few operations on two numbers: 22 and 13
+
+        int number1 = 22;
+        int number2 = 13;
+
+        // Print the result of 13 added to 22
+
+        System.out.println(number1+number2);
+
+        // Print the result of 13 substracted from 22
+
+        System.out.println(number1-number2);
+
+        // Print the result of 22 multiplied by 13
+
+        System.out.println(number1*number2);
+
+        // Print the result of 22 divided by 13 (as a decimal fraction)
+
+        System.out.println((float) number1/ (float) number2);
+
+        // Print the integer part of 22 divided by 13
+
+        System.out.println(number1/number2);
+
+        // Print the reminder of 22 divided by 13
+
+        System.out.println(number1%number2);
+
+    }
+}
