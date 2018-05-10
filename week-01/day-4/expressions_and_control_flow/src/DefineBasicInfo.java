fca156d2a08aee270a6a053ae61ae9a065654f30
juditcsokay <juditcsokay@gmail.com>
2018-05-10 14:23:09 +0200

    VariableMutation

---
+++ week-01/day-4/expressions_and_control_flow/src/DefineBasicInfo.java
@@ -0,0 +1,20 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+
+        String myName = "Jucus";
+        int myAge = 25;
+        double myHeight = 1.75;
+        boolean married = false;
+
+        System.out.println("My name: " + myName);
+        System.out.println("My age: " + myAge);
+        System.out.println("My height: " + myHeight);
+        System.out.println("Am I married? " + married);
+
+    }
+}
