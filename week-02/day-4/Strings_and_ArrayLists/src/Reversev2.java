649d8875424a26cd137f38a1baed6fc21d8f179c
juditcsokay <juditcsokay@gmail.com>
2018-05-15 13:16:39 +0200

    Reverse in the right way

--- week-02/day-4/Strings_and_ArrayLists/src/Reversev2.java
+++ week-02/day-4/Strings_and_ArrayLists/src/Reversev2.java
@@ -1,4 +1,4 @@
-public class Reverse {
+public class Reversev2 {
   public static void main(String... args){
     String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
 
@@ -6,18 +6,15 @@ public class Reverse {
     // Use it on this reversed string to check it!
     // Try to solve this using charAt() first, and optionally anything else after.
 
-    System.out.println(reverseString(reversed));
-
+    System.out.println(reversedString(reversed));
   }
 
-  private static String reverseString(String reversed) {
-    String[] temp = reversed.split("");
-    String goodDirection = "";
-    for (int i = temp.length-1; i >=0 ; i--) {
-      goodDirection = goodDirection.concat(temp[i]);
-      //System.out.println(goodDirection);
+  private static String reversedString(String reversed) {
+    String rightDirection = "";
+    for (int i = reversed.length()-1; i >=0 ; i--) {
+      rightDirection += reversed.charAt(i);
+      //System.out.println(rightDirection);
     }
-    return goodDirection;
+    return rightDirection;
   }
-
 }
