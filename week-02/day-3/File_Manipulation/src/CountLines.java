7672c60c56fa6e9e114321b4c0cfe7a69b3f2c5b
juditcsokay <juditcsokay@gmail.com>
2018-05-16 13:37:11 +0200

    LineCounter

---
+++ week-02/day-3/File_Manipulation/src/CountLines.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CountLines {
+  public static void main(String[] args) {
+    // Write a function that takes a filename as string,
+    // then returns the number of lines the file contains.
+    // It should return zero if it can't open the file, and
+    // should not raise any error.
+
+    System.out.println(numberOfLines("secondfile"));
+  }
+
+  private static int numberOfLines(String filename) {
+    int numberoflines = 0;
+    try {
+      Path filepath = Paths.get(".\\secondfile.txt");
+      List<String> lines = Files.readAllLines(filepath);
+      for (String temp : lines) {
+        numberoflines++;
+      }
+    } catch (IOException e) {
+      System.out.println("Can't read the file!");
+    } catch (Exception e) {
+      System.out.println("Still can't read the file!");
+    }
+    return numberoflines;
+  }
+
+}
