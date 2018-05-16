eb19fde5a31f65417a1174b430e41e766e59f87d
juditcsokay <juditcsokay@gmail.com>
2018-05-16 14:32:45 +0200

    moreLines

---
+++ week-02/day-3/File_Manipulation/src/WriteMultipleLines.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteMultipleLines {
+  public static void main(String[] args) {
+
+    // Create a function that takes 3 parameters: a path, a word and a number,
+    // than it should write to a file.
+    // The path parameter should be a string, that describes the location of the file.
+    // The word parameter should be a string, that will be written to the file as lines
+    // The number paramter should describe how many lines the file should have.
+    // So if the word is "apple" and the number is 5, than it should write 5 lines
+    // to the file and each line should be "apple"
+    // The function should not raise any error if it could not write the file.
+
+    lineWriter(".\\thirdfile.txt", "pacii", 3);
+  }
+
+  private static void lineWriter(String path, String word, int number) {
+
+    try {
+      Path filePath = Paths.get(path);
+      List<String> lines = new ArrayList<>();
+      for (int i = 0; i < number; i++) {
+        lines.add(word);
+        Files.write(filePath, lines);
+      }
+      } catch (Exception e) {
+      System.out.println("Cannot fin the file!");
+      }
+  }
+}
