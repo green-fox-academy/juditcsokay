6d2e17c6588ff371db760e0309ed10ba621ce7e6
juditcsokay <juditcsokay@gmail.com>
2018-05-20 17:26:30 +0200

    ToCenter Dynamic

--- week-02/day-4/Drawing_and_Graphics/src/ToCenter.java
+++ week-02/day-4/Drawing_and_Graphics/src/ToCenter.java
@@ -15,30 +15,31 @@ public class ToCenter {
   }
 
   private static void linesToCenter(int xCoordinate, int yCoordinate, Graphics graphics) {
-      while (xCoordinate != 320 && yCoordinate == 0) {
-        graphics.drawLine(xCoordinate, yCoordinate, 160, 160);
+    int center = WIDTH / 2;
+    while (xCoordinate != WIDTH && yCoordinate == 0) {
+        graphics.drawLine(xCoordinate, yCoordinate, center, center);
         xCoordinate += 20;
       }
-      while (xCoordinate == 320 && yCoordinate != 320) {
-        graphics.drawLine(xCoordinate, yCoordinate, 160, 160);
+      while (xCoordinate == WIDTH && yCoordinate != HEIGHT) {
+        graphics.drawLine(xCoordinate, yCoordinate, center, center);
         yCoordinate += 20;
       }
-      while (xCoordinate != 0 && yCoordinate == 320) {
-        graphics.drawLine(xCoordinate, yCoordinate, 160, 160);
+      while (xCoordinate != 0 && yCoordinate == HEIGHT) {
+        graphics.drawLine(xCoordinate, yCoordinate, center, center);
         xCoordinate -= 20;
       }
       while (xCoordinate == 0 && yCoordinate != 0) {
-        graphics.drawLine(xCoordinate, yCoordinate, 160, 160);
+        graphics.drawLine(xCoordinate, yCoordinate, center, center);
         yCoordinate -= 20;
       }
   }
 
   // Don't touch the code below
   static int WIDTH = 320;
-  static int HEIGHT = 343;
+  static int HEIGHT = 320;
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
-    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT-23));
     jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     jFrame.add(new ImagePanel());
     jFrame.setLocationRelativeTo(null);
