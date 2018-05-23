1b641c04eba099332df6ba5effbc16a78afcb2a3
juditcsokay <juditcsokay@gmail.com>
2018-05-23 16:34:49 +0200

    Animal

---
+++ week-03/day-3/Animal/src/Animal.java
@@ -0,0 +1,26 @@
+public class Animal {
+  int hunger;
+  int thirst;
+
+  public Animal() {
+    hunger = 50;
+    thirst = 50;
+  }
+
+  public void eat () {
+    hunger--;
+  }
+
+  public void drink (){
+    thirst--;
+  }
+
+  public void play () {
+    hunger--;
+    thirst--;
+  }
+
+  public static void main(String[] args) {
+    
+  }
+}
