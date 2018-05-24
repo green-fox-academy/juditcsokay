0cb1498120f3f5273f4c1143ff78dd123e3cd32b
juditcsokay <juditcsokay@gmail.com>
2018-05-24 12:56:48 +0200

    Power

--- week-03/day-4/Recursion/src/Power.java
+++ week-03/day-4/Recursion/src/Power.java
@@ -1,8 +1,28 @@
 public class Power {
 
   public static void main(String[] args) {
-    
 
+    poweringWithLoop(3,4);
+    poweringRecursive(3,3, 3);
+
+  }
+
+  private static int poweringRecursive(int base, int n, int power) {
+    System.out.println(power);
+    power *= base;
+    if (n == 1) {
+      return base;
+    } else {
+      return poweringRecursive(base, n-1, power);
+    }
+  }
+
+  public static void poweringWithLoop(int base, int n) {
+    int power = base;
+    for (int i = 1; i < n; i++) {
+      power *= base;
+      System.out.println(power);
+    }
   }
 
 }
