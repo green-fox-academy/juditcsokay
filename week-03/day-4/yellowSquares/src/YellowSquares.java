7f129a2dc8e1a48a600d7c6174e493250674f8ef
juditcsokay <juditcsokay@gmail.com>
2018-05-25 09:57:20 +0200

    feat(YellowSquares): low solution: first 4 squares

---
+++ week-03/day-4/yellowSquares/src/YellowSquares.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import static javax.swing.WindowConstants.HIDE_ON_CLOSE;
+
+public class YellowSquares {
+
+  public static void mainDraw(Graphics graphics){
+    graphics.setColor(new Color(244,66,66));
+    graphics.fillRect(0,0,WIDTH,HEIGHT);
+
+    for (int i = 0; i < 4; i++) {
+      graphics.setColor(new Color(247,197,57));
+      graphics.drawRect(WIDTH/3, 0, WIDTH/3, HEIGHT/3);
+      graphics.drawRect(0, HEIGHT/3, WIDTH/3, HEIGHT/3);
+      graphics.drawRect(WIDTH/3, HEIGHT/3*2, WIDTH/3, HEIGHT/3);
+      graphics.drawRect(WIDTH/3*2, HEIGHT/3, WIDTH/3, HEIGHT/3);
+
+    }
+  }
+
+
+
+
+  static int WIDTH = 500;
+  static int HEIGHT = 500;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT - 23));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
