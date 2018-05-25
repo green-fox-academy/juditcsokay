52fa3c5f06b6c45fcebe8acb061caa0766253471
juditcsokay <juditcsokay@gmail.com>
2018-05-25 11:55:56 +0200

    feat(YellowSquares): final solution with recursion

--- week-03/day-4/yellowSquares/src/YellowSquares.java
+++ week-03/day-4/yellowSquares/src/YellowSquares.java
@@ -13,40 +13,18 @@ public class YellowSquares {
     graphics.setColor(new Color(247, 197, 57));
 
     getSquares(0,0,WIDTH, graphics);
-
-   /* int tempWidth = WIDTH;
-    int tempHeight = HEIGHT;
-    int x = tempWidth / 3;
-    int y = tempHeight / 3;
-
-    for (int i = 0; i < 5; i++) {
-      for (int j = 0; j < 4; j++) {
-        graphics.setColor(new Color(247, 197, 57));
-        graphics.drawRect(x,0, tempWidth / 3, tempHeight / 3);
-        graphics.drawRect(0, y, tempWidth / 3, tempHeight / 3);
-        graphics.drawRect(x, y * 2, tempWidth / 3, tempHeight / 3);
-        graphics.drawRect(x * 2, y, tempWidth / 3, tempHeight / 3);
-      }
-      tempWidth = tempWidth/3;
-      tempHeight = tempHeight/3;
-      x -= tempWidth;
-      y -= tempHeight;
-    }*/
   }
 
   public static void getSquares(int x, int y, int size, Graphics graphics) {
-    graphics.drawRect(x, y, size, size);
-    if (size < 5) {
-      graphics.drawRect(size,y, size / 3, size / 3);
-      graphics.drawRect(x, size, size / 3, size / 3);
-      graphics.drawRect(x, size * 2, size / 3, size / 3);
-      graphics.drawRect(size * 2, y, size / 3, size / 3);
-    } else {
-      graphics.drawRect(size,y, size / 3, size / 3);
-      graphics.drawRect(x, size, size / 3, size / 3);
-      graphics.drawRect(x, size * 2, size / 3, size / 3);
-      graphics.drawRect(size * 2, y, size / 3, size / 3);
-      getSquares(x,y,size,graphics);
+    graphics.drawRect(x + size/3, y, size / 3, size / 3);
+    graphics.drawRect(x, y + size / 3, size / 3, size / 3);
+    graphics.drawRect(x + size / 3, y + size / 3 * 2, size / 3, size / 3);
+    graphics.drawRect(x + size / 3 * 2, y + size / 3, size / 3, size / 3);
+    if (size > 5) {
+      getSquares(x + size / 3, y, size / 3, graphics);
+      getSquares(x, y + size/3, size / 3, graphics);
+      getSquares(x + size / 3, y + size / 3 *2, size / 3, graphics);
+      getSquares(x + size / 3 *2, y+ size / 3, size / 3, graphics);
     }
   }
 
