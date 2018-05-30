3ad81e11c3a9bbfc9e4a6e226df2225c037be7ba
juditcsokay <juditcsokay@gmail.com>
2018-05-30 12:57:22 +0200

    feat(GreenFoxOrganization): clone method into Student class

---
+++ week-04/day-3/GreenFoxOrganization/src/Sponsor.java
@@ -0,0 +1,34 @@
+public class Sponsor extends Person{
+
+  String company;
+  int hiredStudents;
+
+  public Sponsor(String name, int age, String gender, String company){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this. company = company;
+    this.hiredStudents = 0;
+  }
+
+  public Sponsor(){
+    super();
+    this.company = "Google";
+    this.hiredStudents = 0;
+  }
+
+  @Override
+  public void introduce() {
+    System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + " who represents " + company + " and hired "
+            + hiredStudents  + " students so far.");
+  }
+
+  public void hire() {
+    hiredStudents++;
+  }
+
+  @Override
+  public void getGoal() {
+    System.out.println("Hire brilliant junior software developers.");
+  }
+}
