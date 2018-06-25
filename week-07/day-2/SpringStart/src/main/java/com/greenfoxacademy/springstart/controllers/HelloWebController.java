971782a2a1103f0352645b8b4b5f4702435206bf
juditcsokay <juditcsokay@gmail.com>
2018-06-25 15:54:38 +0200

    Hello World Web App upgraded

--- week-07/day-2/SpringStart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-2/SpringStart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -1,15 +1,22 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import com.greenfoxacademy.springstart.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
 
+  AtomicLong atomicLong = new AtomicLong(1);
+
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World!");
+  public String greeting(Model model1, @RequestParam("name") String name, Model model2) {
+    model1.addAttribute("name", name);
+    model2.addAttribute("id", atomicLong.getAndIncrement());
     return "greeting";
   }
 }
