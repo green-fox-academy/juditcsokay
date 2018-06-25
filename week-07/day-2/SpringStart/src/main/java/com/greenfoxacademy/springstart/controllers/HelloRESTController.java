75df6212ae5c3c17ee857949a274df43cae8dd8d
juditcsokay <juditcsokay@gmail.com>
2018-06-25 15:08:35 +0200

    Hello User Rest App with ATOMIC so the id is increasing

--- week-07/day-2/SpringStart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-2/SpringStart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
+  
+  AtomicLong atomicLong = new AtomicLong(1);
 
   @RequestMapping(value="/greeting")
   public Greeting greeting(@RequestParam("name") String name) {
-    Greeting greeting = new Greeting(1,"Hello, " + name);
+    Greeting greeting = new Greeting(atomicLong.getAndIncrement(),"Hello, " + name);
     return greeting;
   }
 }
