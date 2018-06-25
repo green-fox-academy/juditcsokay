9cc961ec6d768c55b595fa8377396b013be9cb6f
juditcsokay <juditcsokay@gmail.com>
2018-06-25 17:31:10 +0200

    Random language hellos with random size and color

--- week-07/day-2/SpringStart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-2/SpringStart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -11,12 +11,18 @@ import java.util.concurrent.atomic.AtomicLong;
 @Controller
 public class HelloWebController {
 
-  AtomicLong atomicLong = new AtomicLong(1);
+  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+          "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+          "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+          "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+  String[] colors = {"DodgerBlue", "Violet", "Yellow", "Orange", "Tomato", "MediumSeaGreen", "SlateBlue", "GreenYellow", "LightBlue", "Salmon", "Turquoise"};
 
   @RequestMapping("/web/greeting")
-  public String greeting(Model model1, @RequestParam("name") String name, Model model2) {
-    model1.addAttribute("name", name);
-    model2.addAttribute("id", atomicLong.getAndIncrement());
+  public String greeting(Model model) {
+    model.addAttribute("hello", hellos[(int)(Math.random() * hellos.length)]);
+    model.addAttribute("size", (int)(Math.random() * 32) + 20);
+    model.addAttribute("color", colors[(int)(Math.random() * colors.length)]);
     return "greeting";
   }
 }
