59aa4377b6b8f7f5b4209ca4c34e65cfe6d04c45
juditcsokay <juditcsokay@gmail.com>
2018-06-27 15:40:01 +0200

    feat(DependecyInjectionProjekt): showing the different background colored page with service

--- week-07/day-3/DependencyInjection/src/main/java/com/greenfoxacademy/dependencyinjection/controllers/BackgroundColorController.java
+++ week-07/day-3/DependencyInjection/src/main/java/com/greenfoxacademy/dependencyinjection/controllers/BackgroundColorController.java
@@ -1,11 +1,16 @@
 package com.greenfoxacademy.dependencyinjection.controllers;
 
+import com.greenfoxacademy.dependencyinjection.services.UtilityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BackgroundColorController {
+
+  @Autowired
+  UtilityService service;
 
   String[] colors = {"DodgerBlue", "Violet", "Yellow", "Orange", "Tomato", "MediumSeaGreen", "SlateBlue", "GreenYellow", "LightBlue", "Salmon", "Turquoise"};
 
@@ -16,7 +21,7 @@ public class BackgroundColorController {
 
   @RequestMapping("/useful/colored")
   public String greeting(Model model) {
-    model.addAttribute("color", colors[(int)(Math.random() * colors.length)]);
+    model.addAttribute("color", service.randomColor());
     return "colored";
   }
 
